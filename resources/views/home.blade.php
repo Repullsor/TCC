32fe8626cdb3e0781309489d1dae98cc291e9048
Rafael Vasconcelos <rafaelvb_@hotmail.com>
2023-08-20 22:59:16 -0300

    Pacotes de Login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"/>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+    
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12 d-flex justify-content-between">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+        </div>
+
+        
+    </section>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// });
+
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::resource('dashboard', DashboardController::class);
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
